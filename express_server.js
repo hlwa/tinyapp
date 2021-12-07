55fc2238d3193936d5d87808d794886b41e9e0f8
Hailan Wang <hailan6257@email.com>
2021-12-06 20:15:02 -0500

    Add body-parser and short url function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,14 @@
+const generateRandomString = () =>{
+
+};
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,13 +28,23 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  //urlDatabase['body'] = req.body.longURL;
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);//urls_index.ejs, ejs can find file automatically
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
