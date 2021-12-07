ce63797110ccdc97020a274e01fd17c483581546
Hailan Wang <hailan6257@email.com>
2021-12-06 20:50:04 -0500

    Add redirect logic

--- express_server.js
+++ express_server.js
@@ -45,6 +45,11 @@ app.post("/urls", (req, res) => {
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
