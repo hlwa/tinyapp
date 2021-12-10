7a25baa148900dc2da0425ebe28bc5e67e2d5319
Hailan Wang <hailan6257@email.com>
2021-12-09 20:24:19 -0500

    file

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.isTrue(user.id === expectedUserID);
+  });
+
+  it('should return undefined when pass in an email that is not in users database', function() {
+    const user = findUserByEmail("user34@example.com", testUsers);
+    assert.isUndefined(user);
+  });
+});
